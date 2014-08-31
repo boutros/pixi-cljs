a3b190cd6ac66b3bcc0fc3d4493d3c56f6a9888f
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2014-08-31 03:51:55 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# pixi-cljs
+
+Pixi.js bundeled in a JAR for use with clojurescript.
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject goodboydigital/pixi "1.6.1"
+  :description "Pixi.js packaged for Clojurescript"
+  :url "http://www.pixijs.com"
+  :license {:name "MIT"
+            :url "https://github.com/GoodBoyDigital/pixi.js/blob/master/LICENSE"})
