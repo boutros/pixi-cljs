a25b7bf986dc0f256ac572ba5213b44199b1f8dc
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2015-05-02 12:28:58 +0900

    version 2.2.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject goodboydigital/pixi "1.6.1"
+(defproject goodboydigital/pixi "2.2.7"
   :description "Pixi.js packaged for Clojurescript"
   :url "http://www.pixijs.com"
   :license {:name "MIT"
